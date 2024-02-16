4d134ff80b0755f23c051bd16caa669b4343c9f7
nilabja1 <nilabja.work@gmail.com>
2024-02-16 17:34:19 +0530

    managed make login in center and logo to top right corner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div class="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div class="absolute top-0 left-0 px-8 py-2 bg-gradient-to-b from-black z-10">
       <img
-        className="w-60 h-30 right-24"
+        className=" w-60 h-30"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,16 +4,16 @@ import Header from "./Header";
 
 const Login = () => {
   return (
-    <div className="absolute">
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center">
       <Header />
       <img
         src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_small.jpg"
         alt="background"
       />
-      <form className="relative z-10 p-12 bg-black">
-        <input type="email" placeholder="Email Address"></input>
-        <input type="password" placeholder="Password"></input>
-        <button className="p4-m4 bg-red-800">Sign In</button>
+      <form className="w-3/12 absolute p-12 bg-black">
+        <input type="email" placeholder="Email Address" />
+        <input type="password" placeholder="Password" />
+        <button className="p-4 m-4 bg-red-800">Sign In</button>
       </form>
     </div>
   );
